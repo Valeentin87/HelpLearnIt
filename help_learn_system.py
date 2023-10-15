32b38ec63ffd7b0a3114e10d8100d6d6ffdbe256
Valeentin87 <valeentin87@list.ru>
2023-10-15 23:23:10 +0300

    реализовал создание списка объектов типа Questions и переопределил метод __str__

--- help_learn_system.py
+++ help_learn_system.py
@@ -1,3 +1,18 @@
+class Question:
+    '''класс описывает вопросы с их заголовками, формулировками и ответами'''
+    def __init__(self, number_question, title, question, answer) -> None:
+        self.number_question = number_question
+        self.title = title
+        self.question = question
+        self.answer = answer
+    
+    def __str__(self) -> str:
+        return f"Вопрос {self.number_question} Раздел: {self.title} Формулировка: {self.question}"
+
+
+
+
+
 def parser_file(file_path) -> list:
     '''функция принимает относительный путь к файлу, позволяет парсить файл на отдельные вопросы'''
     list_questions = list()
@@ -6,5 +21,23 @@ def parser_file(file_path) -> list:
     list_questions = text_file.split("--@&--")
     return list_questions
 
-HTML_questions = parser_file("HTML_CSS/HTML_CSS_Создание_сайтов.txt")
-print(HTML_questions)
+
+
+
+def create_questions(list):
+    '''создает список объектов типа Questions принимая в качестве параметра распарсенный файл'''
+    questions = []
+    for item in list:
+        item_list = item.split("/&*/")
+        while(len(item_list) == 5):
+            questions.append(Question(item_list[0], item_list[1], item_list[3], item_list[4]))
+            break
+    return questions
+
+
+HTML_parser = parser_file("HTML_CSS/HTML_CSS_Создание_сайтов.txt")
+
+HTML_questions = create_questions(HTML_parser)
+
+for item in HTML_questions:
+    print(item)

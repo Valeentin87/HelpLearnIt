a95bcb69ac76aa285c738a553857a2e458ae1af3
Valeentin87 <valeentin87@list.ru>
2023-10-19 10:22:37 +0300

    добавил картинки

--- interface.py
+++ interface.py
@@ -3,7 +3,7 @@ import help_learn_system as hls
 
 root = Tk()
 root.title("Окно тестирования приложения")
-root.geometry("1200x700")
+root.geometry("1800x700")
 root.resizable(False,False)
 for c in range(6): root.columnconfigure(index=c, weight=1)
 for r in range(3): root.rowconfigure(index=r,weight=1)
@@ -18,22 +18,43 @@ ls_btn = []
 for i in range(1,4):
     for j in range(3):
         if start > len(list_button)-1:
-            ch_btn = Checkbutton(text="Пусто поле", borderwidth=2)
+            ch_btn = Checkbutton(text="Пусто поле")
         else:
             picture_name = "./pictures/"+list_name_pictures[start]
             picture = PhotoImage(file=f"{picture_name}")
-            ch_btn = Checkbutton(text=list_button[start])
+            ch_btn = Checkbutton(text=list_button[start],compound="bottom")
             
             ls_btn.append(ch_btn)
         
-        ch_btn.config(image=picture, compound="bottom")
-        ch_btn.grid(row=i, column=j, sticky="n")
+        
+        ch_btn.grid(row=i, column=j, sticky="ew")
         start+=1
+first_button = ls_btn[0]
+second_button = ls_btn[1]
+third_button = ls_btn[2]
+fourth_button = ls_btn[3]
+five_button = ls_btn[4]
+six_button = ls_btn[5]
+ 
+
+pict1 = PhotoImage(file="./pictures/java.png")
+pict2= PhotoImage(file="./pictures/python3.png")       
+pict3 = PhotoImage(file="./pictures/html.png")
+pict4 = PhotoImage(file="./pictures/db.png")
+pict5 = PhotoImage(file="./pictures/KS.png")
+pict6 = PhotoImage(file="./pictures/linux.png")
+
+first_button.config(image=pict1)
+second_button.config(image=pict2)
+third_button.config(image=pict3)
+fourth_button.config(image=pict4)
+five_button.config(image=pict5)
+six_button.config(image=pict6)
 
         
 
 
-add_question.grid(row=1, column=3, columnspan=4, rowspan=3, sticky="n", padx=2, pady=2)
+add_question.grid(row=1, column=3, columnspan=4, rowspan=1, sticky="n", padx=2, pady=2)
 
 lab_header.grid(column=0, row=0, columnspan=7, padx=4, pady=4, ipadx=5, ipady=5, sticky="nsew", )
 root.mainloop()
